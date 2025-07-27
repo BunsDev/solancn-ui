b5cf9f7ed781e6144c52213ff9f7c4ddba6380a6
Valentina Alexander <bunsthedev@gmail.com>
2025-07-27 07:02:19 -0500

    refactor: convert SolanaDemo to default export in minimal solana starter page

--- src/app/starters/(minimal)/solana/page.tsx
+++ src/app/starters/(minimal)/solana/page.tsx
@@ -156,6 +156,6 @@ const SolanaContent: React.FC = () => {
 }
 
 // Export a wrapper component that provides the wallet context
-export function SolanaDemo() {
+export default function SolanaDemo() {
   return (<SolanaContent />);
 }
