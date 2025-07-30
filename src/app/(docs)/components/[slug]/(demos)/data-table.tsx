e6bc7c31429782642fdc6aa22cdc149eec6a09a3
Valentina Alexander <bunsthedev@gmail.com>
2025-07-29 21:07:15 -0500

    chore: remove unused components and starters from codebase

--- src/app/(docs)/components/[slug]/(demos)/data-table.tsx
+++
@@ -1,8 +0,0 @@
-import { FinanceDataTable } from "@/app/(docs)/components/[slug]/(demos)/data-table-payments";
-
-export const dataTable = {
-  name: "data-table",
-  components: {
-    Default: <FinanceDataTable />,
-  },
-};
